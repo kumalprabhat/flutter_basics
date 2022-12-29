55b0cf5c248fdf3c06eda678b446c1d84be40304
kumal <kumalprabhat@gmail.com>
2022-12-29 12:35:55 +0530

    Navigation Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,59 @@ class _MyLoginPageState extends State<MyLoginPage> {
         title: Text("TextField Example"),
         centerTitle: true,
       ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text("John Doe"),
+              accountEmail: Text("johndoe@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text("J",
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Home",
+                style: TextStyle(color: Colors.black45, fontSize: 16),
+              ),
+              onTap: (){
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Profile",
+              style: TextStyle(color: Colors.black45, fontSize: 16),
+              ),
+              onTap: (){
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("Settings",
+                style: TextStyle(color: Colors.black45, fontSize: 16),
+              ),
+              onTap: (){
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.contacts),
+              title: Text("Contact Us",
+                style: TextStyle(color: Colors.black45, fontSize: 16),
+              ),
+              onTap: (){
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
       body: Padding(
         padding: EdgeInsets.all(16),
         child: Column(
